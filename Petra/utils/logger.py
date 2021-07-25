8c7b514a4d942dd287cce518cf99cd01cb7d501a
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-07-25 18:01:43 +0530

    Update logger.py

--- Petra/utils/logger.py
+++ Petra/utils/logger.py
@@ -1,106 +1,50 @@
-from Petra.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from Petra import DRAGONS
-from telethon.tl.types import ChannelParticipantsAdmins
+# This file is part of Daisy (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from loguru import logger
 
 
-async def user_is_ban_protected(user_id: int, message):
-    status = False
-    if message.is_private or user_id in (IMMUNE_USERS):
-        return True
+class InterceptHandler(logging.Handler):
+    LEVELS_MAP = {
+        logging.CRITICAL: "CRITICAL",
+        logging.ERROR: "ERROR",
+        logging.WARNING: "WARNING",
+        logging.INFO: "INFO",
+        logging.DEBUG: "DEBUG",
+    }
 
-    async for user in telethn.iter_participants(
-            message.chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id:
-            status = True
-            break
-    return status
+    def _get_level(self, record):
+        return self.LEVELS_MAP.get(record.levelno, record.levelno)
 
-
-async def user_is_admin(user_id: int, message):
-    status = False
-    if message.is_private:
-        return True
-
-    async for user in telethn.iter_participants(
-            message.chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in DRAGONS:
-            status = True
-            break
-    return status
+    def emit(self, record):
+        logger_opt = logger.opt(
+            depth=6, exception=record.exc_info, ansi=True, lazy=True
+        )
+        logger_opt.log(self._get_level(record), record.getMessage())
 
 
-async def is_user_admin(user_id: int, chat_id):
-    status = False
-    async for user in telethn.iter_participants(
-            chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in DRAGONS:
-            status = True
-            break
-    return status
-
-
-async def saitama_is_admin(chat_id: int):
-    status = False
-    saitama = await telethn.get_me()
-    async for user in telethn.iter_participants(
-            chat_id, filter=ChannelParticipantsAdmins):
-        if saitama.id == user.id:
-            status = True
-            break
-    return status
-
-
-async def is_user_in_chat(chat_id: int, user_id: int):
-    status = False
-    async for user in telethn.iter_participants(chat_id):
-        if user_id == user.id:
-            status = True
-            break
-    return status
-
-
-async def can_change_info(message):
-    status = False
-    if message.chat.admin_rights:
-        status = message.chat.admin_rights.change_info
-    return status
-
-
-async def can_ban_users(message):
-    status = False
-    if message.chat.admin_rights:
-        status = message.chat.admin_rights.ban_users
-    return status
-
-
-async def can_pin_messages(message):
-    status = False
-    if message.chat.admin_rights:
-        status = message.chat.admin_rights.pin_messages
-    return status
-
-
-async def can_invite_users(message):
-    status = False
-    if message.chat.admin_rights:
-        status = message.chat.admin_rights.invite_users
-    return status
-
-
-async def can_add_admins(message):
-    status = False
-    if message.chat.admin_rights:
-        status = message.chat.admin_rights.add_admins
-    return status
-
-
-async def can_delete_messages(message):
-
-    if message.is_private:
-        return True
-    elif message.chat.admin_rights:
-        status = message.chat.admin_rights.delete_messages
-        return status
-    else:
-        return False
-
+logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO)
+log = logging.getLogger(__name__)
+logger.add(
+    "logs/daisy.log",
+    rotation="1 d",
+    compression="tar.xz",
+    backtrace=True,
+    diagnose=True,
+    level="INFO",
+)
+log.info("Enabled logging intro daisy.log file.")
