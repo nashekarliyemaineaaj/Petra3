63c8ef6d22113859d231b9fdd0e93457cb1cc686
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-08-01 16:17:21 +0530

    Update __init__.py

--- Petra/__init__.py
+++ Petra/__init__.py
@@ -85,6 +85,7 @@ if ENV:
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
     AI_API_KEY = os.environ.get("AI_API_KEY", None)
