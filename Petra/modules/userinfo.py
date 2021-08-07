b92ce813d8de7d39bee4250525f8c0bc73e57e3d
asadali32117 <78930442+asadali32117@users.noreply.github.com>
2021-08-07 11:16:23 +0530

    Create userinfo.py

--- Petra/modules/userinfo.py
+++ Petra/modules/userinfo.py
@@ -271,26 +271,26 @@ def info(update: Update, context: CallbackContext):
     disaster_level_present = False
 
     if user.id == OWNER_ID:
-        text += "\n\nThe Power level of this person is 'Village Founder Senku'."
+        text += "\n\nThe Power level of this person is 'Owner'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThe Power level of this person is 'Science User Chrome'."
+        text += "\n\nThe Power level of this person is 'Dev'."
         disaster_level_present = True
     elif user.id in DRAGONS:
-        text += "\n\nThe Power level of this person is 'Mentalist Gen'."
+        text += "\n\nThe Power level of this person is 'Dragon'."
         disaster_level_present = True
     elif user.id in DEMONS:
-        text += "\n\nThe Power level of this person is 'Archer Ukyo'."
+        text += "\n\nThe Power level of this person is 'Demon'."
         disaster_level_present = True
     elif user.id in TIGERS:
-        text += "\n\nThe Power level of this person is 'Warrior Ginro'."
+        text += "\n\nThe Power level of this person is 'Tiger'."
         disaster_level_present = True
     elif user.id in WOLVES:
-        text += "\n\nThe Power level of this person is 'Village Guard Kinro'."
+        text += "\n\nThe Power level of this person is 'Wolf'."
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/SenkubotUpdates/4">?</a>]'.format(
+        text += ' [<a href="https://t.me/updatesthikhai/1">?</a>]'.format(
             bot.username)
 
     try:
